ee35410fd9080e30a7ab5aa99c5d95c91eab344d
BillyBea1 <billybea853@gmail.com>
2026-05-06 22:24:12 +0200

    Added my first python script

---
+++ calculator.py
@@ -0,0 +1,9 @@
+#A simple rugby score tracker
+tries = 3
+conversions = 2
+
+#Calculations
+total_points = (tries * 5) + (conversions * 2)
+
+print("Match Performance Report:")
+print("Total Points Scored:", total_points)
