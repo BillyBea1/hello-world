efcd63f384ba427cf35eebdfba7148a4b43ddd67
BillyBea1 <billybea853@gmail.com>
2026-05-06 22:57:47 +0200

    Added another update on my first python script

--- calculator.py
+++ calculator.py
@@ -2,9 +2,15 @@
 tries = 5
 conversions = 3
 penalties= 1
+bonus_points = 0
+
+#Logic: If the team scored 4 or more tries, they get a bonus point 
+if tries >= 4
+    bonus_point = 1
+    print("Bonus point earned for 4+ tries!")
 
 #Calculations
-total_points = (tries * 5) + (conversions * 3) + (penalties * 1)
+total_points = (tries * 5) + (conversions * 3) + (penalties * 3) + bonus_point
 
 print("Match Performance Report:")
-print("Total Points Scored:", total_points)
+print("Final Score with Bonus:", total_points)
