fc186c73a07d4f628d7dc439c9191137d4adb2f4
BillyBea1 <billybea853@gmail.com>
2026-05-06 22:37:09 +0200

    Added an update on my first python script

--- calculator.py
+++ calculator.py
@@ -1,9 +1,10 @@
 #A simple rugby score tracker
-tries = 3
-conversions = 2
+tries = 5
+conversions = 3
+penalties= 1
 
 #Calculations
-total_points = (tries * 5) + (conversions * 2)
+total_points = (tries * 5) + (conversions * 3) + (penalties * 1)
 
 print("Match Performance Report:")
 print("Total Points Scored:", total_points)
